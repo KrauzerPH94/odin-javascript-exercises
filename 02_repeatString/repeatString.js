99735a9afd0c5e867808f1bac9c2f375f4a30be5
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-05 02:00:57 -0300

    Solves second exercise using while loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let repStr = "";
+  if (num < 0) {
+    string = "ERROR";
+    repStr = string;
+  } else {
+    while (num > 0) {
+      repStr += string;
+      num--;
+    }
+  }
 
+  return repStr;
 };
 
 // Do not edit below this line
