161ad288cdcec8a43d35ab307a5a8612afa78bfb
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-05 22:05:02 -0300

    Code formating

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,10 @@
- reverseString = function(string) {
-    let revStr = "";
-    let splStr = string.split("");
-    for (let i = splStr.length -1; i >= 0; i--) {
-        revStr += splStr[i];
-    }
-    return revStr;
+reverseString = function (string) {
+  let revStr = "";
+  let splStr = string.split("");
+  for (let i = splStr.length - 1; i >= 0; i--) {
+    revStr += splStr[i];
+  }
+  return revStr;
 };
 
 // Do not edit below this line
