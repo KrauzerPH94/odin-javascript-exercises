c3aa8147f53e62e463d540d4bb533b7124eea914
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-06 00:56:35 -0300

    04_removeFromArray - Remove multiple values test
    For look along with array.inclues() method solved it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-    
+const removeFromArray = function(array, ...element) {
+    let finalArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!element.includes(array[i])) {
+            finalArray.push(array[i]);
+        }
+    }
+    return finalArray;
 };
 
 // Do not edit below this line
