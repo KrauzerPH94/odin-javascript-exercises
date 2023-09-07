fd2cf7b30bc2e085320556c09648d4d6fa964722
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-07 00:11:31 -0300

    Forgot to commit, still trying to figure it out

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,13 @@
 const sumAll = function(start, finish) {
     let sum;
+    let range = [];
     for (let i = start; i <= finish; i++) {
-        sum += start + 1;
+        range += start + 1;
     }
-    return sum;
+    return range;
 };
+
+console.log(sumAll(1, 3));
 
 // Do not edit below this line
 module.exports = sumAll;
