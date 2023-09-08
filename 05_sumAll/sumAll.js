bc6b75dce2500085770cc8c08cb1a3a14c214376
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-07 21:25:06 -0300

    Non-number parameters return "ERROR"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(start, finish) {
     let sum = 0;
     let finalArr = [];
 
-    if (start < 0 || finish < 0) {
+    if (start < 0 || finish < 0 || typeof(start) != "number" || typeof(finish) != "number") {
         return "ERROR";
     } else if (start > finish) {
         for (let i = finish; i <= start; i++) {
