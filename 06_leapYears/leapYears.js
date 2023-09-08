c119464de2b9edf423df9cdf9ebf1114db3ab784
KrauzerPH94 <pkrauzer94@gmail.com>
2023-09-07 22:12:33 -0300

    Leap year validation (%4, %100 and %400)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let validation = false;
+    if (year % 4 === 0 && year % 100 !== 0) {
+        validation = true;
+    } else if (year % 400 === 0) {
+        validation = true;
+    }
+    return validation;
 };
 
 // Do not edit below this line
