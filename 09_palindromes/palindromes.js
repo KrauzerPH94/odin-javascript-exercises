9598f2dd7dd63fc06cd72c867d2a95c98f181396
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-11-03 19:32:43 -0300

    Solution for the 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,9 @@
 const palindromes = function (word) {
-    return word.split('')
-    .map(w => w.split(/(?=\S)/)
-        .reverse()
-        .join(''))
-    .join('');
+    const originalWord = word
+        .toLowerCase() // Solve the 'ZZZ' match (upper case)
+        .replace(/[^a-z0-9]/g, ""); // Solve the '!' match (ponctuation)
+    const reversedWord = originalWord.split('').reverse().join(''); // Reverse the resulting word
+    return originalWord == reversedWord; // Validates if they are 100% equal
 };
 
 // Do not edit below this line
