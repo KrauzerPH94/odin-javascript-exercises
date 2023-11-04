014eab5538276b34a5120f3e43b610e19f2d7311
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-11-04 01:20:34 -0300

    Solution for the first two tests Still need to figure the last one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.sort(function (a, b) {
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    })[arr.length - 1];
 };
 
 // Do not edit below this line
