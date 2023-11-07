1a4db3017d631e2b0aa32f80cf8cb13a2b043eb0
KrauzerPH94 <13894059+KrauzerPH94@users.noreply.github.com>
2023-11-06 22:26:30 -0300

    Solution for the 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,18 @@
+const date = new Date();
+const year = date.getFullYear();
+
 const findTheOldest = function (arr) {
-    return arr.sort(function (a, b) {
-        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
-    })[arr.length - 1];
+    return arr.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = findAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = findAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
+
+const findAge = function (birthYear, deathYear) {
+    if (!deathYear) { deathYear = year };
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
